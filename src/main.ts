82942f99cc81444cdbf51562880d13baabf50367
samimh23 <sami.mahjoub@esprit.tn>
2025-03-09 23:53:52 +0100

    forth

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as express from 'express';
+import * as path from 'path';
+import * as fs from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  const uploadPath = path.join(__dirname, '..', 'uploads');
+
+  // Ensure the uploads folder exists
+  if (!fs.existsSync(uploadPath)) {
+      fs.mkdirSync(uploadPath, { recursive: true });
+  }
+
+  // Serve uploaded files publicly
+  app.use('/uploads', express.static(uploadPath));
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
